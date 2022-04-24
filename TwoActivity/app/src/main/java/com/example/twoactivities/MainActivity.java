3c04057e622ffa66751578454e5ce6b9cd6e1aca
Xeno <17703938+xenoplayed@users.noreply.github.com>
2022-04-25 00:08:03 +0200

    send and return data back between activities

---
+++ TwoActivity/app/src/main/java/com/example/twoactivities/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.twoactivities;
+
+import android.content.Intent;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.example.twoactivity.R;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+    public static final String EXTRA_MESSAGE = "com.example.android.twoactivities.extra.MESSAGE";
+    public static final int TEXT_REQUEST = 1;
+    private EditText mMessageEditText;
+    private TextView mReplyHeadTextView;
+    private TextView mReplyTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mMessageEditText = findViewById(R.id.editText_main);
+        mReplyHeadTextView = findViewById(R.id.text_header_reply);
+        mReplyTextView = findViewById(R.id.text_message_reply);
+    }
+
+    public void launchSecondActivity(View view) {
+        Log.d(LOG_TAG,"Button clicked!");
+        Intent intent = new Intent(this, SecondActivity.class);
+        String message = mMessageEditText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+//        startActivity(intent);
+        startActivityForResult(intent, TEXT_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == TEXT_REQUEST) {
+            if (resultCode == RESULT_OK) {
+                String reply = data.getStringExtra(SecondActivity.EXTRA_REPLY);
+                if (!reply.isEmpty()) {
+                    mReplyHeadTextView.setVisibility(View.VISIBLE);
+                    mReplyTextView.setText(reply);
+                    mReplyTextView.setVisibility(View.VISIBLE);
+                } else {
+                    mReplyHeadTextView.setVisibility(View.INVISIBLE);
+                    mReplyTextView.setVisibility(View.INVISIBLE);
+
+                }
+            }
+        }
+    }
+}
